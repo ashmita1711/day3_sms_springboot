cadf62e29c6550dffdfa917a673e7f70552ec63e
ashmita <ash34973@gmail.com>
2026-01-20 12:48:24 +0530

    updated

---
+++ src/main/java/com/example/day3_sms_springboot/model/StudentModel.java
@@ -0,0 +1,21 @@
+package com.example.day3_sms_springboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data // automatically make getter and setters for everyone
+@AllArgsConstructor //automatically make constructors for everyone
+@NoArgsConstructor // (optional) used for making empty constructor
+@Document(collection = "students")
+
+public class StudentModel {
+    @Id
+    private String id;
+
+    private String name;
+    private int age;
+    private String email;
+}
