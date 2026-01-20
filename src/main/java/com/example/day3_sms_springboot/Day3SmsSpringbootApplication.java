20d1f09c67326833bacae3b5335f8fdcca557968
ashmita <ash34973@gmail.com>
2026-01-20 11:48:20 +0530

    Initial commit

---
+++ src/main/java/com/example/day3_sms_springboot/Day3SmsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.day3_sms_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day3SmsSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Day3SmsSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/day3_sms_springboot/Day3SmsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.day3_sms_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day3SmsSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
