cea30d3d0c76fd216dfd42caf44cb64ea6d63fea
ashmita <ash34973@gmail.com>
2026-02-10 15:59:01 +0530

    create(CRUD) addStudent functionality added

--- src/main/java/com/example/day3_sms_springboot/controller/StudentController.java
+++ src/main/java/com/example/day3_sms_springboot/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.example.day3_sms_springboot.controller;
 import com.example.day3_sms_springboot.dto.StudentRequestDto;
 import com.example.day3_sms_springboot.dto.StudentResponseDto;
 import com.example.day3_sms_springboot.service.StudentService;
+import com.example.day3_sms_springboot.util.JwtUtil;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
@@ -11,20 +12,36 @@ import java.util.Map;
 @RestController
 public class StudentController {
     private final StudentService service;
+    private final JwtUtil jwtUtil;
 
-    public StudentController(StudentService service) {
+    public StudentController(StudentService service, JwtUtil jwtUtil)
+    {
         this.service = service;
+        this.jwtUtil = jwtUtil;
+    }
+
+    private void checkToken(String authHeader){
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            throw new RuntimeException("Invalid token");
+        }
+        String token=authHeader.substring(7);
+        jwtUtil.validateTokenAndGetEmail(token);
     }
 
     //create function API
     @PostMapping("/add-student")
-    public StudentResponseDto addStudent(@Valid @RequestBody StudentRequestDto student){
+    public StudentResponseDto addStudent(
+            @RequestHeader("Authorization") String authHeader,
+            @Valid @RequestBody StudentRequestDto student){
+        checkToken(authHeader);
         return service.addStudent(student);
     }
 
     //Display or read
     @GetMapping("/students")
-    public List<StudentResponseDto> getStudents(){
+    public List<StudentResponseDto> getStudents(
+            @RequestHeader(value = "Authorization", required = false)String authHeader){
+        checkToken(authHeader);
         return service.getAllStudents();
     }
 
---
+++ src/main/java/com/example/day3_sms_springboot/dto/TokenResponseDto.java
@@ -0,0 +1,4 @@
+package com.example.day3_sms_springboot.dto;
+
+public record TokenResponseDto(String Token) {
+}
